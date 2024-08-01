6167a31b2550ffd9705dca2b68b074df40ad1d80
ElenaChugunkina <emudrak.mudrak15@gmail.com>
2024-08-02 01:12:16 +0300

    ready

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect, lazy } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '../Layout';
+import { PrivateRoute } from '../PrivateRoute';
+import { RestrictedRoute } from '../RestrictedRoute';
+import { refreshUser } from '../../redux/auth/operations';
+import { selectIsRefreshing } from '../../redux/auth/selectors';
+import { PropagateLoader } from 'react-spinners'
+import css from '../App/App.module.css'
 
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const RegisterPage = lazy(() => import('../../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() => import('../../pages/ContactsPage/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
